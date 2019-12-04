e71548e50a463f9da97b43c62d9956875ace4524
saki1008 <57945471+saki1008@users.noreply.github.com>
2019-12-04 13:17:12 +0900

    あ

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ) {
+    image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
       var html = `<div class="message" data-message-id=${message.id}>
       <div class="upper-message">
         <div class="upper-message__user-name">
@@ -14,30 +14,13 @@ $(function(){
         <p class="lower-message__content">
           ${message.content}
         </p>
+      ${image} 
       </div>
-      <img src=${message.image} >
     </div>`
     return html;
-  } else {
-    var html =
-     `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
-          </div>
-          <div class="upper-message__date">
-            ${message.date}
-          </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
-    return html;
+  
   };
-}
+
 
 
   
