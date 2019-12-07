23313e57e42d46a7d46d27f4218f13db9113c312
saki1008 <57945471+saki1008@users.noreply.github.com>
2019-12-08 04:39:34 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -48,7 +48,7 @@ $(function(){
 
  var reloadMessages = function() {
   if (window.location.href.match(/\/groups\/\d+\/messages/)){
- var last_message_id = $('.message:last').data("message-id"); 
+ var last_message_id = $('.message:last').data("message-id");
   $.ajax({
     url: "api/messages",
     type: 'get',
